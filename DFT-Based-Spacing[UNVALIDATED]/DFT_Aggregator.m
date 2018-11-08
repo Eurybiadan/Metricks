06b00713e647bc19b038608bf95da325b52a5d5c
Robert F Cooper <robertfcoop@gmail.com>
2018-11-08 11:33:40 -0500

    Switched to averaging across subjects instead of across all available data.

--- DFT-Based-Spacing[UNVALIDATED]/DFT_Aggregator.m
+++ DFT-Based-Spacing[UNVALIDATED]/DFT_Aggregator.m
@@ -15,11 +15,11 @@ for f=1:length(fNames)
     foveal_coords(f,:) = fovea_coords;
     montage_size(f,:) = imsize(1:2);
     
-    montage_rect{f} = [               0                  0; % TLC
-                       montage_size(f,2)                 0; % TRC
+    montage_rect{f} = [               1                  1; % TLC
+                       montage_size(f,2)                 1; % TRC
                        montage_size(f,2) montage_size(f,1); % BRC
-                                      0  montage_size(f,1); % BLC
-                                      0                  0];% TLC 
+                                      1  montage_size(f,1); % BLC
+                                      1                  1];% TLC 
     
     % Shift all montage rectangles to a null point
     montage_rect{f} = montage_rect{f}-foveal_coords(f,:);    
@@ -29,52 +29,65 @@ end
 minglobalbounds = min(cell2mat(montage_rect));
 maxglobalbounds = max(cell2mat(montage_rect));
 
-global_dimension = fliplr(ceil(maxglobalbounds-minglobalbounds)); % Flipped so height/width (row/col), not width/height (x/y)
+global_dimension = fliplr(ceil(maxglobalbounds-minglobalbounds))+1; % Flipped so height/width (row/col), not width/height (x/y)
 
 %% Add all of the dft information to the montage, and combine.
-combined_spacing = zeros(global_dimension);
-combined_error = zeros(global_dimension);
-sum_map = zeros(global_dimension);
+weighted_avg_spacing = zeros(global_dimension);
+weighted_avg_error = zeros(global_dimension);
+combined_sum_map = zeros(global_dimension);
 
-figure;
+% Find the weighted averages of all of the subjects.
 for f=1:length(fNames)
-    load( fullfile(thispath, fNames{f}), 'fovea_coords', 'imbox', 'im_spac_map',...
-                                         'im_err_map', 'im_sum_map');
+    load( fullfile(thispath, fNames{f}), 'fovea_coords', 'imbox', 'blendedim',...
+                                         'blendederrim', 'density_map','sum_map');
     
-    for i=1:length(imbox)
-   
-        thisbox = imbox{i}; % x y width height
-        thisbox(1:2) = round(thisbox(1:2)-fovea_coords-minglobalbounds);
-
-%         tmprect = [thisbox(1)            thisbox(2); % TLC
-%                    thisbox(1)+thisbox(3) thisbox(2); % TRC
-%                    thisbox(1)+thisbox(3) thisbox(2)+thisbox(4); % BRC
-%                    thisbox(1)            thisbox(2)+thisbox(4); % BLC
-%                    thisbox(1)            thisbox(2)]; % TLC
-%         plot(tmprect(:,1), tmprect(:,2)); hold on;    
-
-        thismap = im_spac_map{i};
-        thiserrmap = im_err_map{i};
-        thissummap = im_sum_map{i};
-
-        thismap(isnan(thismap))=0;
-        thiserrmap(isnan(thiserrmap))=0;
-
-        combined_spacing( thisbox(2):thisbox(2)+thisbox(4),...
-                   thisbox(1):thisbox(1)+thisbox(3) ) = combined_spacing( thisbox(2):thisbox(2)+thisbox(4),...
-                                                                   thisbox(1):thisbox(1)+thisbox(3) ) + thismap;
-
-        sum_map( thisbox(2):thisbox(2)+thisbox(4),...
-                 thisbox(1):thisbox(1)+thisbox(3) ) = sum_map( thisbox(2):thisbox(2)+thisbox(4),...
-                                                               thisbox(1):thisbox(1)+thisbox(3) ) + thissummap;
-
-        combined_error( thisbox(2):thisbox(2)+thisbox(4),...
-                   thisbox(1):thisbox(1)+thisbox(3) ) = combined_error( thisbox(2):thisbox(2)+thisbox(4),...
-                                                                   thisbox(1):thisbox(1)+thisbox(3) ) + thiserrmap;                                        
-    end
+    rowrange = round((montage_rect{f}(1,2):montage_rect{f}(3,2))-minglobalbounds(2)+1);
+    colrange = round((montage_rect{f}(1,1):montage_rect{f}(3,1))-minglobalbounds(1)+1);
+    
+    weighted_avg_spacing( rowrange, colrange) = sum(cat(3, weighted_avg_spacing( rowrange, colrange), blendedim),3,'omitnan');
+    weighted_avg_error( rowrange, colrange) = sum(cat(3, weighted_avg_error( rowrange, colrange), blendederrim),3,'omitnan');
+    combined_sum_map( rowrange, colrange) = sum(cat(3, combined_sum_map( rowrange, colrange), blendederrim>0),3,'omitnan');
 end
 
-combined_spacing = combined_spacing./combined_error;
-combined_error = combined_error./sum_map;
+weighted_avg_spacing = weighted_avg_spacing./weighted_avg_error;
 
-imagesc(combined_spacing); 
+
+weighted_avg_spacing_std_dev = zeros(global_dimension);
+weighted_avg_error_std_dev = zeros(global_dimension);
+
+
+% Find the weighted std deviations of all of the subjects.
+for f=1:length(fNames)
+    load( fullfile(thispath, fNames{f}), 'fovea_coords', 'imbox', 'blendedim',...
+                                         'blendederrim', 'density_map','sum_map');
+    
+    rowrange = round((montage_rect{f}(1,2):montage_rect{f}(3,2))-minglobalbounds(2)+1);
+    colrange = round((montage_rect{f}(1,1):montage_rect{f}(3,1))-minglobalbounds(1)+1);
+    
+    % Weighted std deviation equation found here
+    % https://stats.stackexchange.com/questions/6534/how-do-i-calculate-a-weighted-standard-deviation-in-excel
+    weighted_avg_spacing_std_dev( rowrange, colrange) = blendederrim.*(sum(cat(3, blendedim, -weighted_avg_spacing( rowrange, colrange)),3,'omitnan').^2);
+
+end
+
+weighted_std_dev_err_sum = ((combined_sum_map-1)./combined_sum_map).*weighted_avg_error;
+weighted_avg_spacing_std_dev  = sqrt(weighted_avg_spacing_std_dev./weighted_std_dev_err_sum);
+weighted_avg_spacing_std_dev(isinf(weighted_avg_spacing_std_dev)) =0 ;
+weighted_avg_error = weighted_avg_error./combined_sum_map;
+
+% Display the results.
+if kstest(weighted_avg_error(~isnan(weighted_avg_error)))
+    disp('The error is not normally  distributed.')
+    threshold = quantile(weighted_avg_error(~isnan(weighted_avg_error)),0.025)
+    
+else
+    threshold = mean(weighted_avg_error(:),'omitnan')-2*std(weighted_avg_error(:),'omitnan')
+    disp('The error is normally distributed.')
+end
+
+threshold_mask = (weighted_avg_error>threshold);
+
+figure(1); imagesc(weighted_avg_spacing.*threshold_mask); title('Combined Spacing');
+
+figure(2); imagesc(weighted_avg_spacing_std_dev.*threshold_mask); title('Combined Spacing Weighted Std dev');
+figure(3); imagesc(weighted_avg_error.*threshold_mask);
